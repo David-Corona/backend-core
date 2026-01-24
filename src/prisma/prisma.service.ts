f8c15bbc1a252b9aff05fc26779aea3b67cdd15e
David Corona <corona_121@hotmail.com>
2026-01-25 00:42:16 +0100

    added comments

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,10 +3,16 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg'
 
+/* 
+  The Prisma Client is wrapped in a PrismaService. 
+  Single instance for the entire app.
+  Connected on startup, disconnected on shutdown.
+*/
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 
   constructor() {
+    // Prisma Client must be constructed with an adapter that acts as a translator between the Client and the JS database driver
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL!,
     });
